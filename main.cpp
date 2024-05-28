71ff624bcbda170663527a171b86f87939e1a925
likhithkp <likhithkpdev@gmail.com>
2024-05-28 06:48:21 +0530

    If else

--- main.cpp
+++ main.cpp
@@ -2,19 +2,23 @@
 #include <cmath>
 
 int main(){
-    double a;
-    double b;
-    double c;
+    int age ;
 
-    std::cout << "Enter the side A: ";
-    std::cin >> a;
+    std::cout << "Enter your age";
+    std::cin >> age;
 
-    std::cout << "Enter the side B: ";
-    std::cin >> b;
-
-    c = sqrt(pow(a, 2) + pow(b, 2));
-
-    std::cout << "Side C is " << c;
+    if(age >= 100){
+        std::cout << "You are too old";
+    }
+    else if (age >= 18){
+        std::cout << "Welcome to the bar!!";
+    }
+    else if (age < 0){
+        std::cout << "You aren't even born yet";
+    }
+    else{
+        std::cout << "You are not old enough";
+    }
 
     return 0;
 }
