16470198a4292e1b0b15129a4fba5e0bed94d728
likhithkp <likhithkpdev@gmail.com>
2024-05-29 06:20:58 +0530

    Switcched

--- main.cpp
+++ main.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
-#include <cmath>
 
 int main(){
-    int age ;
+    char grade;
 
-    std::cout << "Enter your age";
-    std::cin >> age;
+    std::cout << "Enter your grade";
+    std::cin >> grade;
 
-    if(age >= 100){
-        std::cout << "You are too old";
-    }
-    else if (age >= 18){
-        std::cout << "Welcome to the bar!!";
-    }
-    else if (age < 0){
-        std::cout << "You aren't even born yet";
-    }
-    else{
-        std::cout << "You are not old enough";
+    switch (grade){
+        case 'A':
+            std::cout << "You did great";
+            break;
+        case 'B':
+            std::cout << "You did fine";
+            break;
+        case 'C':
+            std::cout << "Study hard...";
+            break;
+        case 'D':
+            std::cout << "You will be hanged to death for 5 years";
+            break;
+        default:
+            std::cout << "Please enter grade in caps(A-D)";
     }
 
     return 0;
