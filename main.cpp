ddfe9aba8242f36779cebbe063b87fc90810dee2
likhithkp <likhithkpdev@gmail.com>
2024-05-29 07:19:38 +0530

    Temperature conversion program

--- main.cpp
+++ main.cpp
@@ -2,22 +2,23 @@
 
 int main(){
     double temp;
-    bool isSunny = true;
+    char unit;
+    double result;
 
-    std::cout << "Enter the temperature";
+    std::cout << "Enter the unit you need to convert into (C/c, F/f)\n";
+    std::cin >> unit;
+
+    std::cout << "Enter the temperature\n";
     std::cin >> temp;
 
-    if(temp >=0 && temp <= 30){
-        std::cout << "The temperature is great\n";
-    }else{
-        std::cout << "The temperature is bad\n";
-        !isSunny;
-    }
-
-    if(!isSunny){
-        std::cout << "It is not sunny";
-    } else {
-        std::cout << "It is sunny";
+    if(unit == 'F' || unit == 'f'){
+        temp = (1.8 * temp) + 32;
+        std::cout << "The temperature in Fahrenheit is: " << temp << 'F' << '\n';
+    } else if (unit == 'C' || unit =='c'){
+        temp = (temp - 32) / 1.8;
+        std::cout << "The temperature in Celsius is: " << temp << 'C' << '\n';
+    }else {
+        std::cout << "Please enter a valid input";
     }
 
     return 0;
