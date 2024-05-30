7734ac1d25f164afb4559d8c027dafbad5e0010a
likhithkp <likhithkpdev@gmail.com>
2024-05-30 07:07:26 +0530

    nested loops

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 
 int main(){
-    std::string name;
+    int rows;
+    int columns;
+    char symbol;
 
-    while(name.empty()){
-        std::cout << "Please enter your name";
-        std::getline(std::cin, name);
+    std::cout << "Enter the number of rows";
+    std::cin >> rows;
+
+    std::cout << "Enter the number of columns";
+    std::cin >> columns;
+
+    std::cout << "Enter the symbol";
+    std::cin >> symbol;
+
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < columns; j++){
+            std::cout << symbol << " ";
+        }
+        std::cout << '\n';
     }
 
-    std::cout << "Hello" << name;
     return 0;
 }
