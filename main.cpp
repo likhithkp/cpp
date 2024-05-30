477290f192bf34f453bb3bcbcc61d5fc51b5c6b2
likhithkp <likhithkpdev@gmail.com>
2024-05-30 06:40:19 +0530

    while loops and stirng methods

--- main.cpp
+++ main.cpp
@@ -1,25 +1,13 @@
 #include <iostream>
 
 int main(){
-    double temp;
-    char unit;
-    double result;
+    std::string name;
 
-    std::cout << "Enter the unit you need to convert into (C/c, F/f)\n";
-    std::cin >> unit;
-
-    std::cout << "Enter the temperature\n";
-    std::cin >> temp;
-
-    if(unit == 'F' || unit == 'f'){
-        temp = (1.8 * temp) + 32;
-        std::cout << "The temperature in Fahrenheit is: " << temp << 'F' << '\n';
-    } else if (unit == 'C' || unit =='c'){
-        temp = (temp - 32) / 1.8;
-        std::cout << "The temperature in Celsius is: " << temp << 'C' << '\n';
-    }else {
-        std::cout << "Please enter a valid input";
+    while(name.empty()){
+        std::cout << "Please enter your name";
+        std::getline(std::cin, name);
     }
 
+    std::cout << "Hello" << name;
     return 0;
 }
