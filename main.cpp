39b97b6d4601a01e2dc667a1616e220393a59f90
likhithkp <likhithkpdev@gmail.com>
2024-05-30 22:19:54 +0530

    Game

--- main.cpp
+++ main.cpp
@@ -1,25 +1,49 @@
 #include <iostream>
 
+double balance = 10000;
+
+void showBalance(){
+    std::cout << "Your balance is " << balance;
+    return;
+}
+
+double addMoney(double money){
+    std::cout << "Enter the amount to be added \n";
+    std::cin >> money;
+    return balance + money;
+}
+
+double withDraw(double money){
+    std::cout << "Enter the amount to be withdrawn \n";
+    std::cin >> money;
+    return balance - money;
+}
+
 int main(){
-    int rows;
-    int columns;
-    char symbol;
+    double amount = 0;
+    char option = 'x';
 
-    std::cout << "Enter the number of rows";
-    std::cin >> rows;
+    std::cout << "*******************\n";
+    std::cout << "Welome to your bank\n";
+    std::cout << "*******************\n";
+    std::cout << "Select between (b - balance, w - withdraw, a - add, e - exit)\n";
+    std::cin >> option;
 
-    std::cout << "Enter the number of columns";
-    std::cin >> columns;
-
-    std::cout << "Enter the symbol";
-    std::cin >> symbol;
-
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j < columns; j++){
-            std::cout << symbol << " ";
+    do{
+        switch(option){
+            case 'b': showBalance();
+                    break;
+            case 'a': addMoney(amount);
+                    break;
+            case 'w': double result = withDraw(amount);
+                    std::cout << "The updated balance is " << result;
+                    break;
+            case 'e': std::cout << "Thank you for visting your bank\n";
+                    break;
+            default: std::cout << "Not a valid option, select between (b - balance, w - withdraw, a - add, e - exit)\n";
         }
-        std::cout << '\n';
-    }
-
+    }while(option != 'e');
     return 0;
 }
+
+
