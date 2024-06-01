c10827ee1d475492abdf812320346bf35b9b6a60
likhithkp <likhithkpdev@gmail.com>
2024-06-01 16:21:52 +0530

    typedef

--- main.cpp
+++ main.cpp
@@ -1,31 +1,12 @@
-#include <iostream>
+#include<iostream>
 
-int getIndex(std::string array[], int size, std::string element){
-    for(int i = 0; i < size; i++){
-        if(array[i] == element){
-            return i;
-        }
-    }
-    return -1;
-}
+typedef std::string nameSyntax;
 
 int main(){
-    std::string foods[5] = {"burger", "pizza", "paratha", "poori", "idli"};
-    int size = sizeof(foods)/sizeof(std::string);
-    std::string myFood;
-    int index;
+    nameSyntax name;
+    std::cout << "Enter your name\n";
+    std::cin >> name;
 
-    std::cout << "Enter the food\n"; 
-    std::getline(std::cin, myFood);
-
-    index = getIndex(foods, size, myFood);
-
-    if(index != -1 ){
-        std::cout << "The food is in the index " << index; 
-    }else {
-        std::cout << "The food is not in the index\n";
-    }
-
-
-    return 0;
+    std::cout << "Your name is " << name;
+    return  0;
 }
