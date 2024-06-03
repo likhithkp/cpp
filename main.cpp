367e7e2a52ca2345dd8f164000ed1c409b333cee
likhithkp <likhithkpdev.com>
2024-06-03 22:45:36 +0530

    Structs

--- main.cpp
+++ main.cpp
@@ -1,11 +1,34 @@
 #include<iostream>
-template<typename T, typename U>
 
-auto max(T x, U y){
-    return (x > y) ? x : y;
+struct car{
+    std::string brand;
+    std::string name;
+    double mileage;
+    std::string color;
+};
+
+void displayCar(car &x){
+    std::cout << &x << '\n';
+    std::cout << "The car details are" << '\n';
+    std::cout << x.brand << '\n';
+    std::cout << x.name << '\n';
+    std::cout << x.mileage << '\n';
+    std::cout << x.color << '\n';
+}
+
+void paintCar(car &x, std::string color){
+    x.color = color;
 }
 
 int main(){
-    std::cout << max(1, 2.1) << '\n';
+    car bmw;
+    bmw.brand = "BMW";
+    bmw.name = "5 series";
+    bmw.mileage = 9.82;
+    bmw.color = "Metallic blue";
+
+    std::cout << &bmw << '\n';
+    paintCar(bmw, "Gold");
+    displayCar(bmw);
     return 0;
 }
