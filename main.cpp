9eb3853d49e88c78736b6421d4db1964bfcf2817
likhithkp <likhithkpdev.com>
2024-06-03 21:29:15 +0530

    Templates and recursion

--- main.cpp
+++ main.cpp
@@ -1,24 +1,11 @@
 #include<iostream>
+template<typename T, typename U>
+
+auto max(T x, U y){
+    return (x > y) ? x : y;
+}
 
 int main(){
-    int *pPincode = nullptr;
-    int size;
-
-    std::cout << "Enter the size of pincode ";
-    std::cin >> size;
-
-    pPincode = new int[size];
-
-    for(int i = 0; i < size; i++){
-        std::cout << "Enter the number " << i + 1 << '\n';
-        std::cin >> pPincode[i];
-    }
-
-    for(int i = 0; i < size; i++){
-        std::cout << pPincode[i] << '\n';
-    }
-    
-    delete[] pPincode;
-
+    std::cout << max(1, 2.1) << '\n';
     return 0;
 }
