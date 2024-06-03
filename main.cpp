93657eb26634ae443921247c805727eaec8c76b3
likhithkp <likhithkpdev.com>
2024-06-03 19:11:24 +0530

    Practiced dynamic memory and pointers

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include<iostream>
 
 int main(){
-    std::string name = "Likhith";
-    std::string *pName = &name;
-    std::cout << *pName << '\n';
+    int *pPincode = nullptr;
+    int size;
+
+    std::cout << "Enter the size of pincode ";
+    std::cin >> size;
+
+    pPincode = new int[size];
+
+    for(int i = 0; i < size; i++){
+        std::cout << "Enter the number " << i + 1 << '\n';
+        std::cin >> pPincode[i];
+    }
+
+    for(int i = 0; i < size; i++){
+        std::cout << pPincode[i] << '\n';
+    }
+    
+    delete[] pPincode;
+
     return 0;
-};
+}
