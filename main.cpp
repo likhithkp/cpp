4f23ba502f160699e157242b183c7e13791b361c
likhithkp <likhithkpdev.com>
2024-06-03 06:42:01 +0530

    Bubble sort

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include<iostream>
 
-typedef std::string nameSyntax;
+void sort(int arr[], int size){
+    int temp;
+    for(int i = 0; i < size - 1; i++){
+        for(int j = 0; j < size - i - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
 
 int main(){
-    nameSyntax name;
-    std::cout << "Enter your name\n";
-    std::cin >> name;
+    int nums[] = {3, 4, 6, 8, 9, 2, 1, 7, 5, 10};
+    int size = sizeof(nums)/sizeof(int);
+    sort(nums,size);
 
-    std::cout << "Your name is " << name;
-    return  0;
+    for(int num : nums){
+        std::cout << num << "\n";
+    }
+    return 0;
 }
