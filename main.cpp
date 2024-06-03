dd1b0a695c6af3f6a53bc4279cebc709152ee1e9
likhithkp <likhithkpdev.com>
2024-06-03 16:36:26 +0530

    Studied pointers and luhn ago

--- main.cpp
+++ main.cpp
@@ -1,25 +1,8 @@
 #include<iostream>
 
-void sort(int arr[], int size){
-    int temp;
-    for(int i = 0; i < size - 1; i++){
-        for(int j = 0; j < size - i - 1; j++){
-            if(arr[j] > arr[j + 1]){
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-}
-
 int main(){
-    int nums[] = {3, 4, 6, 8, 9, 2, 1, 7, 5, 10};
-    int size = sizeof(nums)/sizeof(int);
-    sort(nums,size);
-
-    for(int num : nums){
-        std::cout << num << "\n";
-    }
+    std::string name = "Likhith";
+    std::string *pName = &name;
+    std::cout << *pName << '\n';
     return 0;
-}
+};
