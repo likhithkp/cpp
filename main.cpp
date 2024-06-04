73509f076e59875a8f1c75d90bc672aff42b4fa7
likhithkp <likhithkpdev.com>
2024-06-04 09:14:00 +0530

    OOPs

--- main.cpp
+++ main.cpp
@@ -1,29 +1,18 @@
 #include<iostream>
 
-class Student{
+class Car{
+    private:
+        void drive(){
+            std::cout << "Car has started and is moving";
+        };
     public:
-        std::string name;
-        int age;
-        std::string branch;
-        std::string usn;
-
-        void greet(){
-            std::cout << "Hello from " << name << '\n';
+        void getDrive(){
+            drive();
         }
 };
 
 int main(){
-    Student student1;
-
-    student1.name = "Shiba Kumar";
-    student1.age = 23;
-    student1.branch = "CSE";
-    student1.usn = "4CP22CS876";
-
-    std::cout << student1.name << '\n';
-    std::cout << student1.age << '\n';
-    std::cout << student1.branch << '\n';
-    std::cout << student1.usn << '\n';
-    student1.greet();
+    Car car1;
+    car1.getDrive();
     return 0;
 }
