92433c9f9b3cf36d1f75f7e8f3aea7b086c84b24
likhithkp <likhithkpdev.com>
2024-06-04 20:15:23 +0530

    List

--- main.cpp
+++ main.cpp
@@ -1,31 +1,14 @@
 #include<iostream>
-#include<deque>
+#include<list>
 
 int main(){
-    std::deque<int> a;
+    std::list<int> l;
+    l.push_front(1);
+    l.push_back(2);
 
-    a.push_front(1);
-    a.push_back(2);
-
-    std::cout << a.front() << '\n';
-    std::cout << a.back() << '\n';
-
-    std::cout << "-------------" << '\n';
-
-    a.pop_back();
-    for(int i : a){
-        std::cout << i << '\n'; 
+    l.erase(l.begin());
+    for(int i: l){
+        std::cout << i << '\n';
     }
-
-    std::cout << "-------------" << '\n';
-
-    std::cout << a.empty() << '\n';
-
-    std::cout << "-------------" << '\n';
-
-    a.erase(a.begin(), a.begin() + 1);
-    std::cout << "---**********--------" << '\n';
-
-    std::cout << a.empty() << '\n';
     return 0;
 }
